2abd88d54dba91e65e73efba89dbfa39ef90e071
Manoj <123manojverma2005@gmail.com>
2025-11-13 00:20:03 +0530

    Added ws initial part

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,32 @@
 import e from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
+import { middleware } from "./middleware.js";
 
 const app=e();
+
+app.post('/signup',(req,res)=>{
+    res.json({
+        userId:123
+    })
+})
+
+app.post('/signin',(req,res)=>{
+
+    const userId=1;
+    const token=jwt.sign({
+        userId
+    },JWT_SECRET);
+    res.json({
+        token
+    })
+})
+
+app.post('/room',middleware,(req,res)=>{
+    res.json({
+        roomId:123
+    })
+})
+
 
 app.listen(3001);
